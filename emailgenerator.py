ce031c3a71016dfeb36dc6d714097b4811d0c598
aval99 <avanishach@gmail.com>
2022-08-14 18:33:02 +0545

    changes

--- emailgenerator.py
+++ emailgenerator.py
@@ -5,11 +5,9 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 count = 0
-i = 0
 b = True
 f = open("email.txt")
 while b:
-    i += 1
     c = f.readline()
     subject = "An email with attachment from Python"
     body = "This is an email with attachment sent from Python"
@@ -35,14 +33,13 @@ while b:
 
     # Log in to server using secure context and send email
     # Log in to server using secure context and send email
-    if c != "":
+    if c == "":
+            b = False
+    else:
         context = ssl.create_default_context()
         with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as server:
             server.login(sender_email, password)
             server.sendmail(sender_email, receiver_email, text) # Log in to server using secure context and send email
             count = count + 1
             print("Mail sent to:", count)
-    else:
-        b = False
 
-print("total mail sent: ", count)
